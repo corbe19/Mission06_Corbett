a4c8674b47186c21332364af19583baa8288aa30
Luke Corbett <corbettluke14@gmail.com>
2025-02-10 10:17:00 -0700

    Add project files.

---
+++ Mission06_Corbett/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission06_Corbett.Models;
+
+namespace Mission06_Corbett.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetToKnowJoel()
+        {
+            return View();
+        }
+
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
